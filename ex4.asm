01306b2c74f54ede25f1c37f59cf5995a0e8d3b8
mazalXD <131989936+mazalXD@users.noreply.github.com>
2023-05-09 06:56:45 +0000

    Add files via upload

--- ex4.asm
+++ ex4.asm
@@ -2,50 +2,37 @@
 
 .section .text
 _start:
-    cmpq $0, head
-    je end
-    movq src, %rax
-    cmpq dst, %rax
-    je end
-    movq head, %rax
-    movq $0, %r8 #previous node src
-    movq %rax, %r9 #pervious node dst
-    movq src, %rsi
-    movq dst, %rdx
 
-search_src:
-    movq %rax, %r10 #src
-    cmpq %rsi, (%rax) #check if src==curr
-    je search_dst
-    cmpq $0, 8(%rax) #check if null
-    je end
-    movq %rax, %r8 #store prev
-    movq 8(%rax), %rax
-    jmp search_src
 
-search_dst:
-    cmpq %rdx, (%rax) #check if dst==curr
-    je swap
-    #leaq 8(%rax), %r15
-    cmpq $0 , 8(%rax) #check if null
-    je end
-    movq %rax, %r9 #store prev
-    movq 8(%rax), %rax
-    jmp search_dst
-     
-swap:#r10=src rax=dst r8=presrc r9=predst
-    cmpq $0, %r8
-    jz no_pre_dst
-    movq %rax, 8(%r8)
-no_pre_dst:
-    movq %r10, 8(%r9)
-    movq 8(%rax), %rbx
-    movq 8(%r10), %rcx
-    movq %rbx, 8(%r10) 
-    movq %rcx, 8(%rax)
-    cmpq %r10, head
-    jne end
-    movq %rax, head
-     
-end:
-    
+# Assume that the head of the linked list is stored in the global variable 'head'
+
+    movq head, %rax    # Load the address of the head node
+    cmpq $0, %rax      # Check if head is null
+    je end_loop        # If so, terminate the loop
+	movl (Value), %rdx
+	
+loop_start:
+    # Process the data in the current node, if necessary
+	movq (%rax), %rcx    # Load the value of the current node into RCX
+	cmpq %rdx, %rcx   # Compare the value with the desired value
+    je found_value
+	
+	
+	
+    movq 8(%rax), %rbx # Load the address of the next node
+    cmpq $0, %rbx      # Check if the next node is null
+    je end_loop        # If so, terminate the loop
+
+    movq %rbx, %rax    # Move to the next node
+    jmp loop_start     # Repeat from the beginning of the loop
+	
+	
+found_value:
+    movl %rdx, (Source)
+    jmp end_loop
+	
+end_loop:
+    # Loop terminated
+
+
+
